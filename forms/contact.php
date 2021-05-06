f36bf73940bd4f73b2b908d4e5595c836d069811
psathish <sathish@qgol.in>
2021-05-06 13:23:28 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -25,10 +25,10 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   $contact->smtp = array(
-    'host' => 'gmail.com',
+    'host' => 'smtp.gmail.com',
     'username' => 'sathish@qgol.in',
     'password' => 's@thish1',
-    'port' => '465'
+    'port' => '587'
   );
 
   $contact->add_message( $_POST['name'], 'From');
